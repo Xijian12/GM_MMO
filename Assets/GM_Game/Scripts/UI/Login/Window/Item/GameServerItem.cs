c5cf28db639cdd2d7a6768d33890fd7340208000
JianXi <3146624068@qq.com>
2026-06-30 19:34:40 +0800

    创建服务器Item脚本

---
+++ Assets/GM_Game/Scripts/UI/Login/Window/Item/GameServerItem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Login
+{
+    /**
+ 	* Title:
+ 	* Desciption:
+ 	**/
+    public class GameServerItem : MonoBehaviour
+    {
+        [SerializeField, Header("运行状态图片")] private Image _imgRunState;
+        [SerializeField, Header("服务器名称")] private TMP_Text _textServerName;
+
+        internal void RefreshUI(GameServer gameServer)
+        {
+            Color color = Color.white;
+            if (gameServer.RunState == 1)
+            {
+                color = Color.red;
+            }
+            else if (gameServer.RunState == 2)
+            {
+                color = Color.yellow;
+            }
+            else if (gameServer.RunState == 3)
+            {
+                color = Color.green;
+            }
+
+            _imgRunState.color = color;
+
+            string str = "";
+            if (gameServer.IsNew == 1)
+            {
+                str = "(新服)";
+            }
+
+            _textServerName.SetText(gameServer.ServerName + str);
+        }
+    }
+}
